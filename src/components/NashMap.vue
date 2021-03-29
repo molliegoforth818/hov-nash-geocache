f0d849badc20483c7a70e8c84a3ea982834e43cf
Mollie Goforth <molliegoforth818@gmail.com>
2021-03-29 11:07:54 -0500

    leaflet installed

---
+++ src/components/NashMap.vue
@@ -0,0 +1,20 @@
+<template>
+  
+</template>
+
+<script>
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet';
+
+export default {
+  name: 'MyAwesomeMap',
+  components: {
+    LMap,
+    LTileLayer,
+    LMarker,
+  },
+};
+</script>
+
+<style>
+
+</style>
